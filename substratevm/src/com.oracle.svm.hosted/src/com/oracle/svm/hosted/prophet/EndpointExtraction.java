8953da36d3680b426c0ae2a5af8b7e2dcdb22036
mr-noah <noah134567@gmail.com>
2023-02-27 22:54:29 -0600

    refactored endpoint extraction code

---
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/EndpointExtraction.java
@@ -0,0 +1,180 @@
+package com.oracle.svm.hosted.prophet;
+
+import com.oracle.graal.pointsto.meta.AnalysisMetaAccess;
+import com.oracle.graal.pointsto.meta.AnalysisMethod;
+import com.oracle.graal.pointsto.meta.AnalysisType;
+import com.oracle.graal.pointsto.meta.AnalysisUniverse;
+import com.oracle.graal.reachability.ReachabilityAnalysisMethod;
+import com.oracle.svm.core.option.HostedOptionKey;
+import com.oracle.svm.hosted.ImageClassLoader;
+import com.oracle.svm.hosted.analysis.Inflation;
+import com.oracle.svm.hosted.prophet.model.Entity;
+import com.oracle.svm.hosted.prophet.model.Field;
+import com.oracle.svm.hosted.prophet.model.Module;
+import com.oracle.svm.hosted.prophet.model.Name;
+import org.graalvm.compiler.graph.Node;
+import org.graalvm.compiler.graph.NodeInputList;
+import org.graalvm.compiler.nodes.CallTargetNode;
+import org.graalvm.compiler.nodes.Invoke;
+import org.graalvm.compiler.nodes.InvokeWithExceptionNode;
+import org.graalvm.compiler.nodes.StructuredGraph;
+import org.graalvm.compiler.nodes.ValueNode;
+import org.graalvm.compiler.options.Option;
+import jdk.vm.ci.meta.ResolvedJavaMethod.Parameter;
+
+
+import java.io.IOException;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class EndpointExtraction {
+
+    private final static String REST_TEMPLATE_PACKAGE = "org.springframework.web.client.RestTemplate.";
+    private final static String PUT_MAPPING = "org.springframework.web.bind.annotation.PutMapping";
+    private final static String GET_MAPPING = "org.springframework.web.bind.annotation.GetMapping";
+    private final static String POST_MAPPING = "org.springframework.web.bind.annotation.PostMapping";
+    private final static String DELETE_MAPPING = "org.springframework.web.bind.annotation.DeleteMapping";
+
+    //annotations for controller to get endpoints
+    private static final Set<String> controllerAnnotationNames = new HashSet<>(Arrays.asList("GetMapping", "PutMapping", "DeleteMapping", "PostMapping"));
+    public static void extractEndpoints(Class<?> clazz, AnalysisMetaAccess metaAccess, Inflation bb) {
+        AnalysisType analysisType = metaAccess.lookupJavaType(clazz);
+        try {
+            for (AnalysisMethod method : analysisType.getDeclaredMethods()) {
+                try {
+
+                    // System.out.println("============");
+
+                    // //Code to get the returnType attribute:
+                    
+
+
+                    // //Code to get the parentMethod attribute:
+                    // Parameter[] params = method.getParameters();
+                    // System.out.println("Annotations for Method: " + method.getQualifiedName());
+                    // String parentMethod = null;
+                    // //following the rad-source format for the parentMethod JSON need to parse before the first parenthesis
+                    // parentMethod = method.getQualifiedName().substring(0,method.getQualifiedName().indexOf("("));
+                    // System.out.println("PARENTSS:: " + parentMethod);
+
+
+
+                    // //Code to get the argument attribute:
+                    // // Example: "arguments": "[@PathVariable Integer id]",
+                    // Annotation[][] annotations1 = method.getParameterAnnotations();
+                    // ArrayList<String> parameterAnnotationsList = new ArrayList<>();
+                    // for (int i = 0; i < annotations1.length; i++) {
+                    //     Annotation[] annotations2 = annotations1[i];
+                    //     for (int j = 0; j < annotations2.length; j++) {
+                    //         Annotation annotation = annotations2[j];
+                    //         //System.out.println("THIS MIGHT BE THE PARAMETER ANNOTATION BRUH: " + annotation.annotationType().getSimpleName());
+                    //         parameterAnnotationsList.add("@" + annotation.annotationType().getSimpleName());
+                    //     }
+                    // }
+
+                    // if(parameterAnnotationsList.size() > 0){
+                    //     int j = 0;
+                    //     for(Parameter p: params){
+                    //         String parameterType = p.getParameterizedType().toString();
+                    //         parameterAnnotationsList.set(j, parameterAnnotationsList.get(j) + " " + 
+                    //         parameterType.substring(parameterType.lastIndexOf(".")+1,parameterType.length()) + " " +
+                    //         p.getName());
+
+                    //         j++;
+                    //         //System.out.println("NAME: " + p.getName() + " ---- PARAMETERIZED TYPE: " + p.getParameterizedType());
+                    //     }
+
+                    //     for(String value : parameterAnnotationsList){
+                    //         System.out.println("argument: " + value);
+                    //     }
+                    // }
+
+                    // System.out.println("============");
+
+                    
+
+                    // //TODO: need to get the root endpoint
+                    // // What I will need to extract; String httpMethod, String parentMethod, String arguments, String returnType
+                    Annotation[] annotations = method.getAnnotations();
+                    for (Annotation annotation : annotations) {
+                        
+                        if (controllerAnnotationNames.contains(annotation.annotationType().getSimpleName())) {
+                            String httpMethod = null, path = null;
+                            if (annotation.annotationType().getName().startsWith(PUT_MAPPING)) {
+                                path = ((String[]) annotation.annotationType().getMethod("value").invoke(annotation))[0];
+                                httpMethod = "PUT";
+                            } else if (annotation.annotationType().getName().startsWith(GET_MAPPING)) {
+                                path = ((String[]) annotation.annotationType().getMethod("value").invoke(annotation))[0];
+                                httpMethod = "GET";
+                            } else if (annotation.annotationType().getName().startsWith(POST_MAPPING)) {
+                                path = ((String[]) annotation.annotationType().getMethod("value").invoke(annotation))[0];
+                                httpMethod = "POST";
+                            } else if (annotation.annotationType().getName().startsWith(DELETE_MAPPING)) {
+                                path = ((String[]) annotation.annotationType().getMethod("value").invoke(annotation))[0];
+                                httpMethod = "DELETE";
+                            }
+
+
+                            //Class<?> returnType = annotation.returnType();
+                            System.out.println("HTTP Method: " + httpMethod + ", Path: " + path + ", ReturnType: ");
+                            
+                            //Special case for request mapping 
+                        }else if (annotation.annotationType().getSimpleName().equals("RequestMapping")){
+                         
+                            String[] pathArr = (String[]) annotation.annotationType().getMethod("path").invoke(annotation);
+                            String path = pathArr.length > 0 ? pathArr[0] : null;
+
+                            //cant use a string[] because of ClassCastException 
+                            Object[] methods = (Object[]) annotation.annotationType().getMethod("method").invoke(annotation);
+                            String httpMethod = null;
+                            if (methods.length > 0 && methods != null) {
+                                httpMethod = methods[0].toString();
+                            }
+
+                            //Class<?> returnType = annotation.returnType();
+                            System.out.println("HTTP Method: " + httpMethod + ", Path: " + path + ", ReturnType: ");
+                        }
+                    }
+
+                    StructuredGraph decodedGraph = ReachabilityAnalysisMethod.getDecodedGraph(bb, method);
+                    for (Node node : decodedGraph.getNodes()) {
+                        if (node instanceof Invoke) {
+                            Invoke invoke = (Invoke) node;
+                            AnalysisMethod targetMethod = ((AnalysisMethod) invoke.getTargetMethod());
+                            if (targetMethod.getQualifiedName().startsWith(REST_TEMPLATE_PACKAGE)) {
+                                System.out.println("Method Qualified Name = " + method.getQualifiedName());
+                                System.out.println("Target Method Qualified Name = " + targetMethod.getQualifiedName());
+                                CallTargetNode callTargetNode = invoke.callTarget();
+                                NodeInputList<ValueNode> arguments = callTargetNode.arguments();
+                                ValueNode zero = arguments.get(0);
+                                ValueNode one = arguments.get(1);
+                                if (one instanceof InvokeWithExceptionNode) {
+                                    // todo figure out when this does not work
+                                    System.out.println("\tFirst arg is invoke:");
+                                    CallTargetNode callTarget = ((InvokeWithExceptionNode) one).callTarget();
+                                    System.out.println(callTarget.targetMethod());
+                                    System.out.println("\targs:");
+                                    for (ValueNode argument : callTarget.arguments()) {
+                                        System.out.println("\targument = " + argument);
+                                    }
+                                }
+                                System.out.println(zero + " " + one);
+                                System.out.println("===");
+                            }
+                        }
+                    }
+                } catch (Exception | LinkageError ex) {
+                    ex.printStackTrace();
+                }
+            }
+        } catch (Exception | LinkageError ex) {
+            ex.printStackTrace();
+        }
+    }
+}
