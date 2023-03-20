ca0011257e5453951924af4933198b6caa08febe
RichardHutcheson <richardhutch80@gmail.com>
2023-03-20 05:38:58 -0500

    add restcall and endpoint to module

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/RestDump.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/RestDump.java
@@ -1,5 +1,10 @@
 package com.oracle.svm.hosted.prophet;
 
 public class RestDump {
-    
+    public static void writeOutRestCalls(List<RestCall> restCalls){
+
+    }
+    public static void writeOutEndpoints(List<Endpoint> endpoints){
+
+    }
 }
