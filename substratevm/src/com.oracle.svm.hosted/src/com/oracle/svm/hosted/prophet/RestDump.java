d47b6f8187f431c4385ea0c841a57ab0ca57dc1d
RichardHutcheson <richardhutch80@gmail.com>
2023-03-20 22:06:55 -0500

    add outfile writing for endpoints and restcalls. add options for those output file names

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/RestDump.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/RestDump.java
@@ -2,13 +2,45 @@ package com.oracle.svm.hosted.prophet;
 import com.oracle.svm.hosted.prophet.model.Endpoint;
 import com.oracle.svm.hosted.prophet.model.RestCall;
 
-import java.util.List;
+import java.io.IOException;
+import java.util.Set;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.lang.RuntimeException;
+
+import java.io.FileWriter;
 
 public class RestDump {
-    public static void writeOutRestCalls(List<RestCall> restCalls){
+    
+    //RESTCALL CSV ORDER SHOULD BE
+    //restCallInClassName, parentMethod, uri, returnType, isCollection
+    //ENDPOINT CSV ORDER SHOULD BE 
+    //endpointInClassName, parentMethod, path, returnType, isCollection, arguments
+
+    public void writeOutRestCalls(Set<RestCall> restCalls, String outputFile){
+
+        if (outputFile == null){
+            throw new RuntimeException("ProphetRestCallOutputFile option was not provided");
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))){
+            for (RestCall rc : restCalls){
+                writer.write(rc.toString() + "\n");
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
 
     }
-    public static void writeOutEndpoints(List<Endpoint> endpoints){
-
+    public void writeOutEndpoints(Set<Endpoint> endpoints, String outputFile){
+        if (outputFile == null){
+            throw new RuntimeException("ProphetEndpointOutputFile option was not provided");
+        }
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))){
+            for (Endpoint ep : endpoints){
+                writer.write(ep.toString() + "\n");
+            }
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
     }
 }
--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/model/Module.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/model/Module.java
@@ -8,32 +8,26 @@ public class Module {
     private Name name;
 
     private Set<Entity> entities;
-    private List<RestCall> restCalls;
-    private List<Endpoint> endpoints;
+    private Set<RestCall> restCalls;
+    private Set<Endpoint> endpoints;
 
-    public Module(Name name, Set<Entity> entities, List<RestCall> restCalls, List<Endpoint> endpoints) {
+    public Module(Name name, Set<Entity> entities, Set<RestCall> restCalls, Set<Endpoint> endpoints) {
         this.name = name;
         this.entities = entities;
         this.restCalls = restCalls;
         this.endpoints = endpoints;
     }
+    
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append("MODULE NAME = ").append(name).append("\n").append("\nENTITIES = \n").append(entitiesToString(entities))
-                .append("\nREST_CALLS = \n").append(listToString(restCalls)).append("\nENDPOINTS = \n" ).append(listToString(endpoints))
+        sb.append("MODULE NAME = ").append(name).append("\n").append("\nENTITIES = \n").append(setToString(entities))
+                .append("\nREST_CALLS = \n").append(setToString(restCalls)).append("\nENDPOINTS = \n" ).append(setToString(endpoints))
                 .append('\n');
         return sb.toString();
     }
 
-    private String entitiesToString(Set<Entity> s){
-        StringBuilder sb = new StringBuilder();
-        for (Entity v : s){
-            sb.append("\t").append(v.toString()).append("\n");
-        }
-        return sb.toString();
-    }
-    private String listToString(List<?> l){
+    private String setToString(Set<?> l){
         StringBuilder sb = new StringBuilder();
         for (var v : l){
             sb.append("\t").append(v.toString()).append("\n");
@@ -57,20 +51,20 @@ public class Module {
     }
 
     // Getter methods
-    public List<RestCall> getRestCalls() {
+    public Set<RestCall> getRestCalls() {
         return restCalls;
     }
 
-    public List<Endpoint> getEndpoints() {
+    public Set<Endpoint> getEndpoints() {
         return endpoints;
     }
 
     // Setter methods
-    public void setRestCalls(List<RestCall> restCalls) {
+    public void setRestCalls(Set<RestCall> restCalls) {
         this.restCalls = restCalls;
     }
 
-    public void setEndpoints(List<Endpoint> endpoints) {
+    public void setEndpoints(Set<Endpoint> endpoints) {
         this.endpoints = endpoints;
     }
 
