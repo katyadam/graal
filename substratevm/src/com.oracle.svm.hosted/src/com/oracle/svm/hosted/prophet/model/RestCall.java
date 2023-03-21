5f459426a057b006c6d70ed5c83931ba96009a9e
RichardHutcheson <richardhutch80@gmail.com>
2023-03-21 01:28:56 -0500

    removed forward whitespace in csvs

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/model/RestCall.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/model/RestCall.java
@@ -22,8 +22,8 @@ public class RestCall {
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(restCallInClassName).append(", ").append(parentMethod).append(", ").append(uri)
-            .append(", ").append(returnType).append(", ").append(isCollection);
+        sb.append(restCallInClassName).append(",").append(parentMethod).append(",").append(uri)
+            .append(",").append(returnType).append(",").append(isCollection);
         return sb.toString();
     }
     // Getter methods
