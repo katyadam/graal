af8d8409f1bd3dfe9794e3eb694b5f1e198db9b7
mr-noah <noah134567@gmail.com>
2023-03-22 00:00:19 -0500

    fixed csv issue

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/EndpointExtraction.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/EndpointExtraction.java
@@ -79,7 +79,7 @@ public class EndpointExtraction {
                     Annotation[] annotations = method.getAnnotations();
                     for (Annotation annotation : annotations) {
                         
-                        ArrayList<String> parameterAnnotationsList = new ArrayList<>();
+                        ArrayList<String> parameterAnnotationsList = null;
                         String httpMethod = null, parentMethod = null, returnTypeResult = null, path = "";
                         boolean returnTypeCollection = false, isEndpoint = false;
                         if (controllerAnnotationNames.contains(annotation.annotationType().getSimpleName())) {
