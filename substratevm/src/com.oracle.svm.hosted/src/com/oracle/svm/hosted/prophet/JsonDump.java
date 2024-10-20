cb7588c3b968483c3802af32301cae845f899d15
Adam Kattan <kattanadam7@gmail.com>
2024-10-20 14:07:12 +0200

    fix: removing last changes

--- substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/JsonDump.java
+++ substratevm/src/com.oracle.svm.hosted/src/com/oracle/svm/hosted/prophet/JsonDump.java
@@ -75,8 +75,6 @@ public class JsonDump {
         Field[] fields = object.getClass().getDeclaredFields();
         for (int i = 0; i < fields.length; i++) {
             Field field = fields[i];
-            if (field.getName().equals("methods"))
-                continue;
             appendString(field.getName());
             append(" : ");
             field.setAccessible(true);
